cbbd8ef54e9e9ea6b6584964fc49dc529adbe91b
NodyHub <jan.harrie@hashicorp.com>
2023-08-14 15:07:08 +0200

    added testcase for extract to external dir

--- test/createArcives.sh
+++ test/createArcives.sh
@@ -7,11 +7,26 @@ prepare_zip() {
   src=$1
   dst=$1.zip
   zip --symlinks -r $testDir/$dst $src
+
+}
+
+prepare_overwrite() {
+  echo "file from upper dir"  > ../traversal
+  zip -r 3_PathtraversalExtract.zip ../traversal
+  rm ../traversal
 }
 
 for dir in *
 do
-  [ -d $dir ] && prepare_zip $dir
+  if [ "$dir" != "$testDir" ]
+  then
+    [ -d $dir ] && prepare_zip $dir
+  fi
 done
+
+# create with ../ filepath
+cd "$testDir"
+prepare_overwrite
+
 
 exit 0
--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,14 @@ cd test-dir-*
 
 for zip in *.zip
 do
+    echo "[!] ../../extract -v $zip "
     ../../extract -v $zip 
 done
+
+echo "################################"
+echo "# Tests performed"
+echo "################################"
+
 
 ls -lR
 cd ..
