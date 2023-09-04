3b0c3f05d5ef5310420901ac28addd7d0fbf4676
NodyHub <jan.harrie@hashicorp.com>
2023-09-04 11:43:34 +0200

    added test

--- unpack_test.go
+++ unpack_test.go
@@ -328,3 +328,46 @@ func TestUnpack(t *testing.T) {
 		})
 	}
 }
+
+func TestMatchesMagicBytes(t *testing.T) {
+
+	// test cases
+	cases := []struct {
+		name        string
+		data        []byte
+		magicBytes  []byte
+		offset      int
+		expectMatch bool
+	}{
+		{
+			name:        "match",
+			data:        []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09},
+			magicBytes:  []byte{0x02, 0x03},
+			offset:      2,
+			expectMatch: true,
+		},
+		{
+			name:        "missmatch",
+			data:        []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09},
+			magicBytes:  []byte{0x02, 0x03},
+			offset:      1,
+			expectMatch: false,
+		},
+	}
+
+	// run cases
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("tc %d", i), func(t *testing.T) {
+
+			// create testing directory
+			expected := tc.expectMatch
+			got := matchesMagicBytes(tc.data, tc.offset, tc.magicBytes)
+
+			// success if both are nil and no engine found
+			if got != expected {
+				t.Errorf("test case %d failed: %s!", i, tc.name)
+			}
+
+		})
+	}
+}
