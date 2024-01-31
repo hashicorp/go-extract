1ec4ae514b1d7e6fcd27c9a16348e012d573968b
NodyHub <jan.harrie@hashicorp.com>
2024-01-31 08:26:45 +0100

    split implementation further

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -70,6 +70,7 @@ var AvailableExtractors = []struct {
 
 var MaxHeaderLength int
 
+// init calculates the maximum header length
 func init() {
 	for _, ex := range AvailableExtractors {
 		needs := ex.Offset
@@ -82,40 +83,6 @@ func init() {
 			MaxHeaderLength = needs
 		}
 	}
-}
-
-// HeaderReader is an implementation of io.Reader that allows the first bytes of
-// the reader to be read twice. This is useful for identifying the archive type
-// before unpacking.
-type HeaderReader struct {
-	r      io.Reader
-	header []byte
-}
-
-func NewHeaderReader(r io.Reader, headerSize int) (*HeaderReader, error) {
-	// read at least headerSize bytes. If EOF, capture whatever was read.
-	buf := make([]byte, headerSize)
-	n, err := io.ReadFull(r, buf)
-	if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
-		return nil, err
-	}
-	return &HeaderReader{r, buf[:n]}, nil
-}
-
-func (p *HeaderReader) Read(b []byte) (int, error) {
-	// read from header first
-	if len(p.header) > 0 {
-		n := copy(b, p.header)
-		p.header = p.header[n:]
-		return n, nil
-	}
-
-	// then continue reading from the source
-	return p.r.Read(b)
-}
-
-func (p *HeaderReader) PeekHeader() []byte {
-	return p.header
 }
 
 func matchesMagicBytes(data []byte, offset int, magicBytes [][]byte) bool {
@@ -134,48 +101,6 @@ func matchesMagicBytes(data []byte, offset int, magicBytes [][]byte) bool {
 
 	// no match found
 	return false
-}
-
-// limitErrorReaderCounter is a reader that returns an error if the limit is exceeded
-// before the underlying reader is fully read.
-// If the limit is -1, all data from the original reader is read.
-type limitErrorReaderCounter struct {
-	R io.Reader // underlying reader
-	L int64     // limit
-	N int64     // number of bytes read
-}
-
-// Read reads from the underlying reader and fills up p.
-// It returns an error if the limit is exceeded, even if the underlying reader is not fully read.
-// If the limit is -1, all data from the original reader is read.
-// Remark: Even if the limit is exceeded, the buffer p is filled up to the max or until the underlying
-// reader is fully read.
-func (l *limitErrorReaderCounter) Read(p []byte) (int, error) {
-
-	// read from underlying reader
-	n, err := l.R.Read(p)
-	l.N += int64(n)
-	if err != nil {
-		return n, err
-	}
-
-	// check if limit has exceeded
-	if l.L >= 0 && l.N > l.L {
-		return n, fmt.Errorf("read limit exceeded")
-	}
-
-	// return
-	return n, nil
-}
-
-// ReadBytes returns how many bytes have been read from the underlying reader
-func (l *limitErrorReaderCounter) ReadBytes() int {
-	return int(l.N)
-}
-
-// newLimitErrorReaderCounter returns a new limitErrorReaderCounter that reads from r
-func newLimitErrorReaderCounter(r io.Reader, limit int64) *limitErrorReaderCounter {
-	return &limitErrorReaderCounter{R: r, L: limit, N: 0}
 }
 
 // handleError increases the error counter, sets the latest error and
