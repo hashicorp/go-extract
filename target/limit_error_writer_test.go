126a0a7321c14ef9c2b0360bccebd29aaa60d5db
NodyHub <jan.harrie@hashicorp.com>
2024-04-04 16:27:12 +0200

    extended test cases

--- target/limit_error_writer_test.go
+++ target/limit_error_writer_test.go
@@ -9,11 +9,16 @@ import (
 // TestLimitErrorWriter tests the LimitErrorWriter
 func TestLimitErrorWriter(t *testing.T) {
 	var buf bytes.Buffer
-	l := NewLimitErrorWriter(&buf, 5)
+	l := NewLimitErrorWriter(&buf, 8)
 
-	n, err := l.Write([]byte("hello"))
+	n, err := l.Write([]byte("Hello"))
 	if n != 5 || err != nil {
 		t.Errorf("Expected to write 5 bytes, but wrote %d with error %v", n, err)
+	}
+
+	n, err = l.Write([]byte("World"))
+	if n != 3 || err != io.ErrShortWrite {
+		t.Errorf("Expected to write 3 bytes and get ErrShortWrite, but wrote %d with error %v", n, err)
 	}
 
 	n, err = l.Write([]byte("world"))
@@ -21,7 +26,7 @@ func TestLimitErrorWriter(t *testing.T) {
 		t.Errorf("Expected to write 0 bytes and get ErrShortWrite, but wrote %d with error %v", n, err)
 	}
 
-	if buf.String() != "hello" {
-		t.Errorf("Expected buffer to contain 'hello', but it contains '%s'", buf.String())
+	if buf.String() != "HelloWor" {
+		t.Errorf("Expected buffer to contain 'HelloWor', but it contains '%s'", buf.String())
 	}
 }
