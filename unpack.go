8a02c210483c16cabee0da58990d520c35d511f3
NodyHub <jan.harrie@hashicorp.com>
2024-04-15 11:23:49 +0200

    added enforcement for untar after decomporession of tgz file

--- unpack.go
+++ unpack.go
@@ -20,6 +20,9 @@ func Unpack(ctx context.Context, src io.Reader, dst string, c *config.Config) er
 	// check if type is set
 	if len(c.ExtractType()) > 0 {
 		if ae, found := extractor.AvailableExtractors[c.ExtractType()]; found {
+			if c.ExtractType() == extractor.FileTypeTarGZip {
+				c.SetNoUntarAfterDecompression(false)
+			}
 			return ae.Unpacker(ctx, src, dst, c)
 		}
 
