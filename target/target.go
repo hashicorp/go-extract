917138a63e14ee9b06f932f570c6ae0dd9d7cea8
Jan Harrie <NodyHub@users.noreply.github.com>
2024-04-25 11:52:08 +0200

    Update target/target.go
    Co-authored-by: Kent Gruber <kent.picat.gruber@gmail.com>

--- target/target.go
+++ target/target.go
@@ -10,13 +10,26 @@ import (
 // Target specifies all function that are needed to be implemented to extract contents from an archive
 type Target interface {
 
-	// CreateSafeFile is used to create a file in directory dstDir, with name and reader as content.
-	// dstDir is the directory where the file should be created. If dstDir is empty, the file is meant to
-	// be created in the current working directory. If dstDir does not exist and config.CreateDestination() returns
-	// true, it might be created with the config.CustomCreateDirMode() by the implementation. The mode parameter is the
-	// file mode that should be set on the file.
-	// If the path of the file contains path traversal, an error should be returned. If the path *to the file* (not dstDir) does not
-	// exist, the directories should be created with the config.CustomCreateDirMode() by the implementation.
+	// CreateSafeFile creates a file with a specified name and content within a given directory.
+	// The function takes a configuration object, a destination directory, a name for the file, 
+	// a reader for the file content, and a file mode.
+	//
+	// The dstDir parameter specifies the directory where the file should be created. If dstDir is empty,
+	// the file is created in the current working directory. If dstDir does not exist and config.CreateDestination()
+	// returns true, the function may create the directory with permissions specified by config.CustomCreateDirMode().
+	//
+	// The name parameter specifies the name of the file. If the name contains path traversal (e.g., "../"),
+	// the function returns an error to prevent the creation of files outside of the intended directory.
+	//
+	// The reader parameter provides the content for the file.
+	//
+	// The mode parameter specifies the file mode that should be set on the file.
+	//
+	// If the path to the file (excluding dstDir) does not exist, the function may create the necessary directories
+	// with permissions specified by config.CustomCreateDirMode().
+	//
+	// The function returns an error if there's a problem creating the file or the necessary directories.
+	// If the function completes successfully, it returns nil.
 	CreateSafeFile(config *config.Config, dstDir string, name string, reader io.Reader, mode fs.FileMode) error
 
 	// CreateSafeDir creates dirName in dstDir. If dstDir is empty, the directory is meant to be created in the current working directory.
