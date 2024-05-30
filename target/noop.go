b68dce5c589d19b44f8b581c999839beb4db7cd4
NodyHub <jan.harrie@hashicorp.com>
2024-05-30 13:17:16 +0200

    aligned targets

--- target/noop.go
+++ target/noop.go
@@ -17,16 +17,16 @@ func NewNoopTarget() *Noop {
 }
 
 // CreateSafeDir does nothing.
-func (n *Noop) CreateSafeDir(dstBase string, newDir string, mode fs.FileMode, config *config.Config) error {
+func (n *Noop) CreateSafeDir(dst string, name string, mode fs.FileMode, cfg *config.Config) error {
 	return nil
 }
 
 // CreateSafeFile does nothing.
-func (n *Noop) CreateSafeFile(dstDir string, name string, reader io.Reader, mode fs.FileMode, config *config.Config) error {
+func (n *Noop) CreateSafeFile(dst string, name string, src io.Reader, mode fs.FileMode, cfg *config.Config) error {
 	return nil
 }
 
 // CreateSafeSymlink does nothing.
-func (n *Noop) CreateSafeSymlink(dstDir string, name string, linkTarget string, config *config.Config) error {
+func (n *Noop) CreateSafeSymlink(dst string, name string, target string, cfg *config.Config) error {
 	return nil
 }
