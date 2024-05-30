e74bb8c8e0df86979eb32184b302500946fb9de4
NodyHub <jan.harrie@hashicorp.com>
2024-05-30 14:08:48 +0200

    adjusted param names

--- extractor/target.go
+++ extractor/target.go
@@ -9,16 +9,16 @@ import (
 )
 
 // createFile is a wrapper around the target.CreateFile function
-func createFile(t target.Target, dstDir string, name string, reader io.Reader, mode fs.FileMode, config *config.Config) error {
-	return t.CreateSafeFile(dstDir, name, reader, mode, config)
+func createFile(t target.Target, dst string, name string, src io.Reader, mode fs.FileMode, cfg *config.Config) error {
+	return t.CreateSafeFile(dst, name, src, mode, cfg)
 }
 
 // createDir is a wrapper around the target.CreateDir function
-func createDir(t target.Target, dstDir string, dirName string, mode fs.FileMode, config *config.Config) error {
-	return t.CreateSafeDir(dstDir, dirName, mode, config)
+func createDir(t target.Target, dst string, name string, mode fs.FileMode, cfg *config.Config) error {
+	return t.CreateSafeDir(dst, name, mode, cfg)
 }
 
 // createSymlink is a wrapper around the target.CreateSymlink function
-func createSymlink(t target.Target, dstDir string, name string, linkTarget string, config *config.Config) error {
-	return t.CreateSafeSymlink(dstDir, name, linkTarget, config)
+func createSymlink(t target.Target, dst string, name string, linkTarget string, cfg *config.Config) error {
+	return t.CreateSafeSymlink(dst, name, linkTarget, cfg)
 }
