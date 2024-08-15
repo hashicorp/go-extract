b2dd0c61b0976b1ef4c57e6bb571d969a6462e74
NodyHub <jan.harrie@hashicorp.com>
2024-08-15 10:21:36 +0200

    added code comments

--- target/memory.go
+++ target/memory.go
@@ -8,10 +8,16 @@ import (
 	"time"
 )
 
+// NewMemory creates a new in-memory filesystem
 func NewMemory() Memory {
 	return make(Memory)
 }
 
+// CreateFile creates a new file in the in-memory filesystem. The file is created with the given mode.
+// If the overwrite flag is set to false and the file already exists, an error is returned. If the overwrite
+// flag is set to true, the file is overwritten. The maxSize parameter can be used to limit the size of the file.
+// If the file exceeds the maxSize, an error is returned. If the file is created successfully, the number of bytes
+// written is returned.
 func (m Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwrite bool, maxSize int64) (int64, error) {
 	if !overwrite {
 		if _, ok := m[path]; ok {
@@ -39,6 +45,9 @@ func (m Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwri
 	return n, nil
 }
 
+// CreateDir creates a new directory in the in-memory filesystem.
+// If the directory already exists, nothing is done. If the directory does not exist, it is created.
+// The directory is created with the given mode. If the directory is created successfully, nil is returned.
 func (m Memory) CreateDir(path string, mode fs.FileMode) error {
 	if _, ok := m[path]; ok {
 		return nil
@@ -51,6 +60,9 @@ func (m Memory) CreateDir(path string, mode fs.FileMode) error {
 	return nil
 }
 
+// CreateSymlink creates a new symlink in the in-memory filesystem.
+// If the overwrite flag is set to false and the symlink already exists, an error is returned.
+// If the overwrite flag is set to true, the symlink is overwritten. If the symlink is created successfully, nil is returned.
 func (m Memory) CreateSymlink(oldName string, newName string, overwrite bool) error {
 	if !overwrite {
 		if _, ok := m[newName]; ok {
@@ -66,6 +78,9 @@ func (m Memory) CreateSymlink(oldName string, newName string, overwrite bool) er
 	return nil
 }
 
+// Open opens a file in the in-memory filesystem. The file is returned as a ReadCloser
+// which can be used to read the file contents. If the file does not exist, an error is returned.
+// If the file is opened successfully, the ReadCloser is returned.
 func (m Memory) Open(path string) (io.ReadCloser, error) {
 	if e, ok := m[path]; ok {
 		return io.NopCloser(bytes.NewReader(e.Data)), nil
@@ -73,6 +88,8 @@ func (m Memory) Open(path string) (io.ReadCloser, error) {
 	return nil, fs.ErrNotExist
 }
 
+// Lstat returns the FileInfo for the given path. If the path is a symlink, the FileInfo for the symlink is returned.
+// If the path does not exist, an error is returned.
 func (m Memory) Lstat(path string) (fs.FileInfo, error) {
 	if e, ok := m[path]; ok {
 		return e.FileInfo, nil
@@ -80,6 +97,8 @@ func (m Memory) Lstat(path string) (fs.FileInfo, error) {
 	return nil, fs.ErrNotExist
 }
 
+// Stat returns the FileInfo for the given path. If the path is a symlink, the FileInfo for the target of the symlink is returned.
+// If the path does not exist, an error is returned.
 func (m Memory) Stat(path string) (fs.FileInfo, error) {
 	if e, ok := m[path]; ok {
 		if m[path].FileInfo.Mode()&fs.ModeSymlink != 0 {
@@ -91,13 +110,16 @@ func (m Memory) Stat(path string) (fs.FileInfo, error) {
 	return nil, fs.ErrNotExist
 }
 
+// Memory is an in-memory filesystem implementation
 type Memory map[string]MemoryEntry
 
+// MemoryEntry is an entry in the in-memory filesystem
 type MemoryEntry struct {
 	FileInfo fs.FileInfo
 	Data     []byte
 }
 
+// MemoryFileInfo is a FileInfo implementation for the in-memory filesystem
 type MemoryFileInfo struct {
 	name    string
 	size    int64
@@ -105,26 +127,32 @@ type MemoryFileInfo struct {
 	modTime time.Time
 }
 
+// Name returns the name of the file
 func (fi MemoryFileInfo) Name() string {
 	return fi.name
 }
 
+// Size returns the size of the file
 func (fi MemoryFileInfo) Size() int64 {
 	return fi.size
 }
 
+// Mode returns the mode of the file
 func (fi MemoryFileInfo) Mode() fs.FileMode {
 	return fi.mode
 }
 
+// ModTime returns the modification time of the file
 func (fi MemoryFileInfo) ModTime() time.Time {
 	return fi.modTime
 }
 
+// IsDir returns true if the file is a directory
 func (fi MemoryFileInfo) IsDir() bool {
 	return fi.mode.IsDir()
 }
 
+// Sys returns the underlying data source (nil for in-memory filesystem)
 func (fi MemoryFileInfo) Sys() any {
 	return nil
 }
