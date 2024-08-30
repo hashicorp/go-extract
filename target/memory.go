7654464a0acd784d3f667f0fec0a5405ac90d298
NodyHub <jan.harrie@hashicorp.com>
2024-08-30 15:27:22 +0200

    adopted sync.Map

--- target/memory.go
+++ target/memory.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -18,13 +19,13 @@ import (
 // not enforced. Entries can be accessed by the path as a key in the map, or by calling
 // the m.Open(<path>) function.
 type Memory struct {
-	files map[string]*MemoryEntry
+	files sync.Map // map[string]*MemoryEntry
 }
 
 // NewMemory creates a new in-memory filesystem.
 func NewMemory() Target {
 	return &Memory{
-		files: make(map[string]*MemoryEntry),
+		files: sync.Map{},
 	}
 }
 
@@ -33,12 +34,12 @@ func NewMemory() Target {
 // flag is set to true, the file is overwritten. The maxSize parameter can be used to limit the size of the file.
 // If the file exceeds the maxSize, an error is returned. If the file is created successfully, the number of bytes
 // written is returned.
-func (m Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwrite bool, maxSize int64) (int64, error) {
+func (m *Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwrite bool, maxSize int64) (int64, error) {
 	if !fs.ValidPath(path) {
 		return 0, fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
 	if !overwrite {
-		if _, ok := m.files[path]; ok {
+		if _, ok := m.files.Load(path); ok {
 			return 0, fmt.Errorf("%w: %s", fs.ErrExist, path)
 		}
 	}
@@ -53,12 +54,12 @@ func (m Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwri
 		return n, err
 	}
 
-	fName := filepath.Base(path)
 	// create entry
-	m.files[path] = &MemoryEntry{
+	fName := filepath.Base(path)
+	m.files.Store(path, &MemoryEntry{
 		FileInfo: &MemoryFileInfo{name: fName, size: n, mode: mode.Perm(), modTime: time.Now()},
 		Data:     buf.Bytes(),
-	}
+	})
 
 	// return number of bytes written
 	return n, nil
@@ -67,19 +68,21 @@ func (m Memory) CreateFile(path string, src io.Reader, mode fs.FileMode, overwri
 // CreateDir creates a new directory in the in-memory filesystem.
 // If the directory already exists, nothing is done. If the directory does not exist, it is created.
 // The directory is created with the given mode. If the directory is created successfully, nil is returned.
-func (m Memory) CreateDir(path string, mode fs.FileMode) error {
+func (m *Memory) CreateDir(path string, mode fs.FileMode) error {
 	if !fs.ValidPath(path) {
 		return fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
 
-	if _, ok := m.files[path]; ok {
+	// check if an entry already exists
+	if _, ok := m.files.Load(path); ok {
 		return nil
 	}
 
+	// create entry
 	dName := filepath.Base(path)
-	m.files[path] = &MemoryEntry{
+	m.files.Store(path, &MemoryEntry{
 		FileInfo: &MemoryFileInfo{name: dName, mode: mode.Perm() | fs.ModeDir},
-	}
+	})
 
 	return nil
 }
@@ -87,21 +90,21 @@ func (m Memory) CreateDir(path string, mode fs.FileMode) error {
 // CreateSymlink creates a new symlink in the in-memory filesystem.
 // If the overwrite flag is set to false and the symlink already exists, an error is returned.
 // If the overwrite flag is set to true, the symlink is overwritten. If the symlink is created successfully, nil is returned.
-func (m Memory) CreateSymlink(oldName string, newName string, overwrite bool) error {
+func (m *Memory) CreateSymlink(oldName string, newName string, overwrite bool) error {
 	if !fs.ValidPath(newName) {
 		return fmt.Errorf("%w: %s", fs.ErrInvalid, newName)
 	}
 	if !overwrite {
-		if _, ok := m.files[newName]; ok {
+		if _, ok := m.files.Load(newName); ok {
 			return fmt.Errorf("%w: %s", fs.ErrExist, newName)
 		}
 	}
 
 	lName := filepath.Base(newName)
-	m.files[newName] = &MemoryEntry{
+	m.files.Store(newName, &MemoryEntry{
 		FileInfo: &MemoryFileInfo{name: lName, mode: 0777 | fs.ModeSymlink},
 		Data:     []byte(oldName),
-	}
+	})
 
 	return nil
 }
@@ -110,13 +113,13 @@ func (m Memory) CreateSymlink(oldName string, newName string, overwrite bool) er
 // as an [io.ReadCloser] which can be used to read the file contents. If the
 // file is  a symlink, the target of the symlink is opened. If the file does not
 // exist, or is a directory, an error is returned.
-func (m Memory) Open(path string) (fs.File, error) {
+func (m *Memory) Open(path string) (fs.File, error) {
 	if !fs.ValidPath(path) {
 		return nil, fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
 
 	// get entry
-	e, ok := m.files[path]
+	e, ok := m.files.Load(path)
 
 	// file does not exist
 	if !ok {
@@ -124,66 +127,70 @@ func (m Memory) Open(path string) (fs.File, error) {
 	}
 
 	// handle directory
-	if e.FileInfo.Mode()&fs.ModeDir != 0 {
+	me := e.(*MemoryEntry)
+	if me.FileInfo.Mode()&fs.ModeDir != 0 {
 		return nil, fmt.Errorf("cannot open directory")
 	}
 
 	// handle symlink
-	if e.FileInfo.Mode()&fs.ModeSymlink != 0 {
-		linkTarget := string(e.Data)
+	if me.FileInfo.Mode()&fs.ModeSymlink != 0 {
+		linkTarget := string(me.Data)
 		linkTarget = filepath.Join(filepath.Dir(path), linkTarget)
 		return m.Open(linkTarget)
 	}
 
 	// create copy of entry
-	e = &MemoryEntry{
-		FileInfo: e.FileInfo,
-		Data:     e.Data,
+	me = &MemoryEntry{
+		FileInfo: me.FileInfo,
+		Data:     me.Data,
 	}
 
 	// return file data
-	return e, nil
+	return me, nil
 
 }
 
 // Lstat returns the FileInfo for the given path. If the path is a symlink, the FileInfo for the symlink is returned.
 // If the path does not exist, an error is returned.
-func (m Memory) Lstat(path string) (fs.FileInfo, error) {
+func (m *Memory) Lstat(path string) (fs.FileInfo, error) {
 	if !fs.ValidPath(path) {
 		return nil, fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
-	if e, ok := m.files[path]; ok {
-		return e.FileInfo, nil
+	if e, ok := m.files.Load(path); ok {
+		me := e.(*MemoryEntry)
+		return me.FileInfo, nil
 	}
 	return nil, fmt.Errorf("%w: %s", fs.ErrNotExist, path)
 }
 
 // Stat returns the FileInfo for the given path. If the path is a symlink, the FileInfo for the target of the symlink is returned.
 // If the path does not exist, an error is returned.
-func (m Memory) Stat(path string) (fs.FileInfo, error) {
+func (m *Memory) Stat(path string) (fs.FileInfo, error) {
 	if !fs.ValidPath(path) {
 		return nil, fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
-	if e, ok := m.files[path]; ok {
-		if m.files[path].FileInfo.Mode()&fs.ModeSymlink != 0 {
-			linkTarget := string(m.files[path].Data)
+	if e, ok := m.files.Load(path); ok {
+		me := e.(*MemoryEntry)
+		if me.FileInfo.Mode()&fs.ModeSymlink != 0 {
+			linkTarget := string(me.Data)
 			linkTarget = filepath.Join(filepath.Dir(path), linkTarget)
 			return m.Stat(linkTarget)
 		}
-		return e.FileInfo, nil
+		return me.FileInfo, nil
 	}
 	return nil, fmt.Errorf("%w: %s", fs.ErrNotExist, path)
 }
 
 // Readlink returns the target of the symlink at the given path. If the path is not a symlink, an error is returned.
 // If the path does not exist, an error is returned. If the symlink exists, the target of the symlink is returned.
-func (m Memory) Readlink(path string) (string, error) {
+func (m *Memory) Readlink(path string) (string, error) {
 	if !fs.ValidPath(path) {
 		return "", fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
-	if e, ok := m.files[path]; ok {
-		if e.FileInfo.Mode()&fs.ModeSymlink != 0 {
-			return string(e.Data), nil
+	if e, ok := m.files.Load(path); ok {
+		me := e.(*MemoryEntry)
+		if me.FileInfo.Mode()&fs.ModeSymlink != 0 {
+			return string(me.Data), nil
 		}
 		return "", fmt.Errorf("not a symlink: %w: %s", fs.ErrInvalid, path)
 	}
@@ -191,27 +198,28 @@ func (m Memory) Readlink(path string) (string, error) {
 }
 
 // Remove removes the entry at the given path. If the path does not exist, an error is returned.
-func (m Memory) Remove(path string) error {
+func (m *Memory) Remove(path string) error {
 	if !fs.ValidPath(path) {
 		return fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
-	delete(m.files, path)
+	m.files.Delete(path)
 	return nil
 }
 
-func (m Memory) ReadDir(path string) ([]fs.DirEntry, error) {
+func (m *Memory) ReadDir(path string) ([]fs.DirEntry, error) {
 	if !fs.ValidPath(path) {
 		return nil, fmt.Errorf("%w: %s	", fs.ErrInvalid, path)
 	}
 
 	// get all entries in the directory
 	var entries []fs.DirEntry
-	for k, v := range m.files {
-		if filepath.Dir(k) == path {
-
-			entries = append(entries, v)
+	m.files.Range(func(entryPath, me any) bool {
+		if filepath.Dir(entryPath.(string)) == path {
+			entries = append(entries, me.(*MemoryEntry))
 		}
-	}
+		return true
+	})
+
 	// sort slice of entries based on name
 	sort.Slice(entries, func(i, j int) bool {
 		return entries[i].Name() < entries[j].Name()
@@ -220,15 +228,16 @@ func (m Memory) ReadDir(path string) ([]fs.DirEntry, error) {
 	return entries, nil
 }
 
-func (m Memory) ReadFile(path string) ([]byte, error) {
+func (m *Memory) ReadFile(path string) ([]byte, error) {
 	if !fs.ValidPath(path) {
 		return nil, fmt.Errorf("%w: %s", fs.ErrInvalid, path)
 	}
-	if e, ok := m.files[path]; ok {
-		if e.FileInfo.Mode()&fs.ModeDir != 0 {
+	if e, ok := m.files.Load(path); ok {
+		me := e.(*MemoryEntry)
+		if me.FileInfo.Mode()&fs.ModeDir != 0 {
 			return nil, fmt.Errorf("cannot read directory")
 		}
-		return e.Data, nil
+		return me.Data, nil
 	}
 	return nil, fmt.Errorf("%w: %s", fs.ErrNotExist, path)
 }
@@ -239,20 +248,15 @@ func (m *Memory) Sub(dir string) (fs.FS, error) {
 		return nil, fmt.Errorf("%w: %s", fs.ErrInvalid, dir)
 	}
 
-	// Check if the directory exists
-	e, ok := m.files[dir]
-	if !ok || !e.IsDir() {
-		return nil, fmt.Errorf("%w: %s", fs.ErrNotExist, dir)
-	}
-
 	// Create a new Memory filesystem for the subdirectory
 	dir = filepath.Clean(dir) + "/"
 	subFS := NewMemory().(*Memory)
-	for path, entry := range m.files {
-		if strings.HasPrefix(path, dir) {
-			subFS.files[path[len(dir):]] = entry
+	m.files.Range(func(path, entry any) bool {
+		if strings.HasPrefix(path.(string), dir) {
+			subFS.files.Store(path.(string)[len(dir):], entry)
 		}
-	}
+		return true
+	})
 
 	return subFS, nil
 }
@@ -264,15 +268,16 @@ func (m *Memory) Glob(pattern string) ([]string, error) {
 	}
 
 	var matches []string
-	for path := range m.files {
-		match, err := filepath.Match(pattern, path)
+	m.files.Range(func(path, entry any) bool {
+		match, err := filepath.Match(pattern, path.(string))
 		if err != nil {
-			return nil, err
+			return false
 		}
 		if match {
-			matches = append(matches, path)
+			matches = append(matches, path.(string))
 		}
-	}
+		return true
+	})
 
 	return matches, nil
 }
