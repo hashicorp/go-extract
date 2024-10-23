d160c8ffee862176f45fe3ec51ec172cee7d257e
NodyHub <jan.harrie@hashicorp.com>
2024-10-23 12:15:03 +0200

    added statement re secure by default

--- config/config.go
+++ config/config.go
@@ -2,6 +2,9 @@
 //
 // The configuration struct holds all configuration options for the extraction process.
 // The configuration options can be adjusted using the option pattern style.
+//
+// The default configuration is designed to be secure by default and prevent exhaustion,
+// path traversal and symlink attacks.
 package config
 
 import (
