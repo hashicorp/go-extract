a4cc8fb2e85e43cbf1cf59797e17583475023f56
NodyHub <jan.harrie@hashicorp.com>
2024-10-24 14:39:22 +0200

    adjusted documentation according to kents recommendation

--- doc.go
+++ doc.go
@@ -1,10 +1,7 @@
 // Package extract provides a function to extract files from a reader to a destination.
 //
-// The extraction is done according to the file type, and the package provides a default extraction target OS.
-// Available extractors are defined in the internal/extractor package. The package also provides an interface
-// for the target that must be implemented to perform the unpacking process. This packages provides as well a
-// memory target that provides an in-memory filesystem. The package also provides errors that can be returned
-// during the unpacking process.
+// The extraction process is determined by the file type, with support for various formats
+// that can be output to the underlying OS, in-memory, or a custom filesystem target.
 //
 // Configuration is done using the [config] package, which provides a configuration struct that can be used to
 // set the extraction type, the logger, the telemetry hook, and the maximum input size. Telemetry data is captured
