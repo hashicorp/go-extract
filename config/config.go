b8626c6f671f0cbec4a9549bab19d31d0a6836ec
NodyHub <jan.harrie@hashicorp.com>
2024-10-24 18:22:16 +0200

    relocated package documentation

--- config/config.go
+++ config/config.go
@@ -1,10 +1,3 @@
-// Package config provides a configuration struct and options to adjust the configuration.
-//
-// The configuration struct holds all configuration options for the extraction process.
-// The configuration options can be adjusted using the option pattern style.
-//
-// The default configuration is designed to be secure by default and prevent exhaustion,
-// path traversal and symlink attacks.
 package config
 
 import (
